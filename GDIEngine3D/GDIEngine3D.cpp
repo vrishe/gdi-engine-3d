f0e00990025da18fc32480bc6e5c177541d9baf5
vlados.v10.0 <vlados.v10.0@1d85ebff-84cf-3366-91f6-5f09b1c39448>
2012-09-18 20:44:02 +0000

    Some improvements and fixes.

--- GDIEngine3D/GDIEngine3D.cpp
+++ GDIEngine3D/GDIEngine3D.cpp
@@ -6,69 +6,62 @@
 #include "thread_safety.h"
 #include "Render.h"
 
-#define _IS_HANDLE_VALID(type_name, handle_name)											\
-	handle_name != NULL																		\
-	&& thread_safety::AccessorExists((LPMUTUAL_ACCESSIBLE)handle_name)						\
-	&& ((LPMUTUAL_ACCESSIBLE)handle_name)->getObjectTypeInfo() == typeid(type_name)
+void accessor_cleanup(LPUNKNOWN lpObject) { delete lpObject; }
 
+inline size_t CreateObject(LPUNKNOWN lpObject)
+{
+	thread_safety::LockModule(INFINITE);
+	size_t objectID = thread_safety::RegisterObject(lpObject);
+	thread_safety::UnlockModule();
 
-void accessor_cleanup(LPMUTUAL_ACCESSIBLE lpAcObj) { delete lpAcObj; }
+	return objectID;
+}
+
+inline LPUNKNOWN ReleaseObject(size_t uObjectIndex)
+{
+	thread_safety::LockModule(INFINITE);
+	LPUNKNOWN underlier = thread_safety::DeregisterObject(uObjectIndex);
+	thread_safety::UnlockModule();
+
+	return underlier;
+}
+
+// ============================================================================
+// Common object interface implementation
+
+BOOL WINAPI EngineObjectRelease(HGDIENGINE3DOBJ hGDIEngineObject)
+{
+	LPUNKNOWN lpObject;
+
+	delete (lpObject = ReleaseObject((size_t)hGDIEngineObject));
+
+	return lpObject != NULL;
+}
 
 // ============================================================================
 // CRenderPool library interface implementation
 
-HRENDERPOOL3D WINAPI CreateRenderPool3D()
+HRENDERPOOL WINAPI RenderPoolCreate()
 {
-	// Step 1: Object Initialization
-	LPMUTUAL_ACCESSIBLE newborn = new MUTUAL_ACCESSIBLE(new RENDER_POOL());
-	// Step 2: Library lock	
-	thread_safety::LockModule(INFINITE);
-	// Step 3: Write object ptr to Library
-	thread_safety::AddAccessor(newborn);
-	// Step 4: Unlock library
-	thread_safety::UnlockModule();
-	// Step 5: return object handle
-	return (HRENDERPOOL3D)newborn;
+	return (HRENDERPOOL)CreateObject(new RENDER_POOL());
 }
 
-BOOL WINAPI ReleaseRenderPool3D(HRENDERPOOL3D hRp3D)
+BOOL WINAPI RenderPoolAssignScene(HRENDERPOOL hRenderPool, HSCENE hScene)
 {
-	LPMUTUAL_ACCESSIBLE victim = NULL;
+	BOOL bResult;
 
-	// Step 1: Lock library
-	thread_safety::LockModule(INFINITE);
-	// Step 2: Check handle
-	if ( _IS_HANDLE_VALID(RENDER_POOL, hRp3D) )
-	{
-		// Step 3: If VALID - erase handle from the list
-		victim = (LPMUTUAL_ACCESSIBLE)hRp3D;
-		thread_safety::RemoveAccessor(victim);
-		// Step 4: if VALID - lock and delete victim object
-		delete victim->Lock();
-		delete victim;
-	}
-	// Step 6: Unlock Library
-	thread_safety::UnlockModule();
-	// Step 7: return operation result
-	return victim != NULL;
-}
-
-BOOL WINAPI RenderPoolAssignScene3D(HRENDERPOOL3D hRp3D, HSCENE3D hSc3D)
-{
-	BOOL bResult = FALSE;
-
-	LPUNKNOWN RenderPool	= NULL;
-	LPUNKNOWN Scene			= NULL;
+	LPUNKNOWN Master = NULL;
+	LPUNKNOWN Slave	 = NULL;
 
 	// Step 1: Lock library
 	thread_safety::LockModule(INFINITE);
 	// Step 2: Check handle(s)
-	if ( bResult = _IS_HANDLE_VALID(RENDER_POOL, hRp3D) 
-		&& _IS_HANDLE_VALID(SCENE3D, hSc3D) )
+	if ( bResult = thread_safety::IsObjectRegistered((size_t)hRenderPool, typeid(RENDER_POOL)) 
+		&& thread_safety::IsObjectRegistered((size_t)hScene, typeid(SCENE3D)) )
 	{
 		// Step 3: Lock objects(s)
-		RenderPool	= ((LPMUTUAL_ACCESSIBLE)hRp3D)->Lock();
-		Scene		= ((LPMUTUAL_ACCESSIBLE)hSc3D)->Lock();
+		thread_safety::LockObjectRegistered((size_t)hRenderPool, Master);
+		thread_safety::LockObjectRegistered((size_t)hScene, Slave);
 	}
 	// Step 4: Unlock library 
 	thread_safety::UnlockModule();
@@ -76,217 +69,191 @@ BOOL WINAPI RenderPoolAssignScene3D(HRENDERPOOL3D hRp3D, HSCENE3D hSc3D)
 	if (bResult)
 	{
 		// Step 5: Make necessary adjustments
-		((LPRENDER_POOL)RenderPool)->assignScene((LPSCENE3D)Scene);
+		bResult = ((LPRENDER_POOL)Master)->assignScene((LPSCENE3D)Slave);
 		// Step 6: Unlock object(s)
-		((LPMUTUAL_ACCESSIBLE)hRp3D)->Unlock();
-		((LPMUTUAL_ACCESSIBLE)hSc3D)->Unlock();
+		thread_safety::UnlockObjectRegistered((size_t)hRenderPool, Master);
+		thread_safety::UnlockObjectRegistered((size_t)hScene, Slave);
 	}
-	// Step 7: Return operatiob result
+	// Step 7: Return operation result
 	return bResult;
 }
-
-// TODO: Implement the most useful interface functions, such as: CreateLight, CreateCamera, RenderWorld(CRenderPool based) and so on 
 
 // ============================================================================
 // CScene library interface implementation
 
-/* Constructor () */
-HSCENE3D WINAPI CreateScene3D() 
+HSCENE WINAPI SceneCreate() 
 {
-	LPMUTUAL_ACCESSIBLE newborn = new MUTUAL_ACCESSIBLE(new SCENE3D());
-	thread_safety::LockModule(INFINITE);
-	thread_safety::AddAccessor(newborn);
-	thread_safety::UnlockModule();
-	return (HSCENE3D)newborn;
+	return (HSCENE)CreateObject(new SCENE3D());
 }
 
-/* Destructor */
-BOOL WINAPI ReleaseScene3D(HSCENE3D hSc3D)
+BOOL WINAPI SceneClear(HSCENE hScene)
 {
-	LPMUTUAL_ACCESSIBLE victim = NULL;
+	BOOL bResult;
+
+	LPUNKNOWN Master = NULL;
 
 	thread_safety::LockModule(INFINITE);
-	if ( _IS_HANDLE_VALID(RENDER_POOL, hSc3D) )
-	{
-		victim = (LPMUTUAL_ACCESSIBLE)hSc3D;
-		thread_safety::RemoveAccessor(victim);
-		delete victim->Lock();
-		delete victim;
-	}
-	thread_safety::UnlockModule();
-	return victim != NULL;
-}
 
-/* AddObject (const LPOBJECT3D lpObject) */
-BOOL WINAPI SceneAddObject3D(HSCENE3D hSc3D, HOBJECT3D hObj3D)
-{
-	BOOL bResult = FALSE;
+	if (bResult = thread_safety::IsObjectRegistered((size_t)hScene, typeid(SCENE3D)))
+		thread_safety::LockObjectRegistered((size_t)hScene, Master);
 
-	LPUNKNOWN Scene			= NULL;
-	LPUNKNOWN Object		= NULL;
-
-	thread_safety::LockModule(INFINITE);
-	if ( bResult = _IS_HANDLE_VALID(SCENE3D, hSc3D) 
-		&& _IS_HANDLE_VALID(OBJECT3D, hObj3D) )
-	{
-		Scene	= ((LPMUTUAL_ACCESSIBLE)hSc3D)->Lock();
-		Object	= ((LPMUTUAL_ACCESSIBLE)hObj3D)->Lock();
-	}
 	thread_safety::UnlockModule();
 
 	if (bResult)
 	{
-		bResult = ((LPSCENE3D)Scene)->AddObject((LPOBJECT3D)Object);
-		((LPMUTUAL_ACCESSIBLE)hObj3D)->Unlock();
-		((LPMUTUAL_ACCESSIBLE)hSc3D)->Unlock();
+		((LPSCENE3D)Master)->Clear();
+
+		thread_safety::UnlockObjectRegistered((size_t)hScene, Master);
 	}
 
 	return bResult;
 }
 
-/* DeleteObject (size_t objID) */
-BOOL WINAPI SceneDeleteObject3D(HSCENE3D hSc3D, HOBJECT3D hObj3D)
-{
-	BOOL bResult = FALSE;
+//
+//UINT_PTR WINAPI SceneGetObjectClassCount(HSCENE hScene, CLASS_ID clsID) 
+//{
+//	BOOL isValid;
+//
+//	LPUNKNOWN Master = NULL;
+//
+//	thread_safety::LockModule(INFINITE);
+//
+//	if (isValid = thread_safety::IsObjectRegistered((size_t)hScene, typeid(SCENE3D)))
+//		thread_safety::LockObjectRegistered((size_t)hScene, Master);
+//
+//	thread_safety::UnlockModule();
+//
+//	UINT_PTR uResult = UINT_MAX;
+//	if (isValid)
+//	{
+//		((LPSCENE3D)Master)->getObjectClassCount(clsID);
+//
+//		thread_safety::UnlockObjectRegistered((size_t)hScene, Master);
+//	}
+//
+//	return uResult;
+//}
 
-	LPUNKNOWN Scene			= NULL;
-	LPUNKNOWN Object		= NULL;
+BOOL WINAPI SceneSetAmbientColor(HSCENE hScene, COLORREF color)
+{
+	BOOL bResult;
+
+	LPUNKNOWN Master = NULL;
 
 	thread_safety::LockModule(INFINITE);
-	if ( bResult = _IS_HANDLE_VALID(SCENE3D, hSc3D) 
-		&& _IS_HANDLE_VALID(OBJECT3D, hObj3D) )
-	{
-		Scene	= ((LPMUTUAL_ACCESSIBLE)hSc3D)->Lock();
-		Object	= ((LPMUTUAL_ACCESSIBLE)hObj3D)->Lock();
-	}
+
+	if (bResult = thread_safety::IsObjectRegistered((size_t)hScene, typeid(SCENE3D)))
+		thread_safety::LockObjectRegistered((size_t)hScene, Master);
+
 	thread_safety::UnlockModule();
 
 	if (bResult)
 	{
-		// TODO: Implement another DeleteObject methods if necessary
-		bResult = ((LPSCENE3D)Scene)->DeleteObject(((LPOBJECT3D)Object)->objID);
-		((LPMUTUAL_ACCESSIBLE)hObj3D)->Unlock();
-		((LPMUTUAL_ACCESSIBLE)hSc3D)->Unlock();
+		((LPSCENE3D)Master)->setAmbientColor(color);
+
+		thread_safety::UnlockObjectRegistered((size_t)hScene, Master);
 	}
 
 	return bResult;
 }
 
-/* Clear () */
-BOOL WINAPI SceneClear3D(HSCENE3D hSc3D)
+BOOL WINAPI SceneGetAmbientColor(HSCENE hScene, COLORREF &color)
 {
-	BOOL bResult = FALSE;
+	BOOL bResult;
 
-	LPUNKNOWN Scene	= NULL;
+	LPUNKNOWN Master = NULL;
 
 	thread_safety::LockModule(INFINITE);
-	if ( bResult = _IS_HANDLE_VALID(RENDER_POOL, hSc3D) )
-	{
-		Scene	= ((LPMUTUAL_ACCESSIBLE)hSc3D)->Lock();
-	}
+
+	if (bResult = thread_safety::IsObjectRegistered((size_t)hScene, typeid(SCENE3D)))
+		thread_safety::LockObjectRegistered((size_t)hScene, Master);
+
 	thread_safety::UnlockModule();
 
 	if (bResult)
 	{
-		((LPSCENE3D)Scene)->Clear();
-		((LPMUTUAL_ACCESSIBLE)hSc3D)->Unlock();
+		color = ((LPSCENE3D)Master)->getAmbientColor();
+
+		thread_safety::UnlockObjectRegistered((size_t)hScene, Master);
 	}
 
 	return bResult;
 }
 
-/* getObjectClassCount (CLASS_ID clsID) */
-UINT_PTR WINAPI SceneGetObjectClassCount3D(HSCENE3D hSc3D, CLASS_ID clsID) 
-{
-	BOOL isValid = FALSE;
-	UINT_PTR uResult = UINT_MAX;
-
-	LPUNKNOWN Scene			= NULL;
-
-	thread_safety::LockModule(INFINITE);
-	if ( isValid = _IS_HANDLE_VALID(SCENE3D, hSc3D) )
-	{
-		Scene	= ((LPMUTUAL_ACCESSIBLE)hSc3D)->Lock();
-	}
-	thread_safety::UnlockModule();
-
-	if (isValid)
-	{
-		uResult = ((LPSCENE3D)Scene)->getObjectClassCount(clsID);
-		((LPMUTUAL_ACCESSIBLE)hSc3D)->Unlock();
-	}
-
-	return uResult;
-}
-
-/* getFirstObject (CLASS_ID *objID) */
-BOOL WINAPI SceneGetFirstObject3D(HSCENE3D hSc3D, CLASS_ID clsID)
-{
-	BOOL bResult = FALSE;
-
-	LPUNKNOWN Scene			= NULL;
-
-	thread_safety::LockModule(INFINITE);
-	if ( bResult = _IS_HANDLE_VALID(SCENE3D, hSc3D) )
-	{
-		Scene	= ((LPMUTUAL_ACCESSIBLE)hSc3D)->Lock();
-	}
-	thread_safety::UnlockModule();
-
-	if (bResult)
-	{
-		bResult = ((LPSCENE3D)Scene)->getFirstObject(&clsID);
-		((LPMUTUAL_ACCESSIBLE)hSc3D)->Unlock();
-	}
-
-	return bResult;
-}
-
-/* getAmbientColor () */
-COLORREF WINAPI SceneGetAmbientColor3D(HSCENE3D hSc3D)
-{
-	BOOL isValid = FALSE;
-	COLORREF crResult = ULONG_MAX;
-
-	LPUNKNOWN Scene	= NULL;
-
-	thread_safety::LockModule(INFINITE);
-	if ( isValid = _IS_HANDLE_VALID(RENDER_POOL, hSc3D) )
-	{
-		Scene	= ((LPMUTUAL_ACCESSIBLE)hSc3D)->Lock();
-	}
-	thread_safety::UnlockModule();
-
-	if (isValid)
-	{
-		crResult = ((LPSCENE3D)Scene)->getAmbientColor();
-		((LPMUTUAL_ACCESSIBLE)hSc3D)->Unlock();
-	}
-
-	return crResult;
-}
-
-/* setAmbientColor (COLORREF color) */
-BOOL WINAPI SceneSetAmbientColor3D(HSCENE3D hSc3D, COLORREF color)
-{
-	BOOL bResult = FALSE;
-
-	LPUNKNOWN Scene	= NULL;
-
-	thread_safety::LockModule(INFINITE);
-	if ( bResult = _IS_HANDLE_VALID(RENDER_POOL, hSc3D) )
-	{
-		Scene	= ((LPMUTUAL_ACCESSIBLE)hSc3D)->Lock();
-	}
-	thread_safety::UnlockModule();
-
-	if (bResult)
-	{
-		((LPSCENE3D)Scene)->setAmbientColor(color);
-		((LPMUTUAL_ACCESSIBLE)hSc3D)->Unlock();
-	}
-
-	return bResult;
-}
+//BOOL WINAPI SceneAddObject(HSCENE hScene, HOBJECT3D hObj3D)
+//{
+//	BOOL bResult = FALSE;
+//
+//	LPUNKNOWN Scene			= NULL;
+//	LPUNKNOWN Object		= NULL;
+//
+//	thread_safety::LockModule(INFINITE);
+//	if ( bResult = _IS_HANDLE_VALID(SCENE3D, hScene) 
+//		&& _IS_HANDLE_VALID(OBJECT3D, hObj3D) )
+//	{
+//		Scene	= ((LPMUTUAL_ACCESSIBLE)hScene)->Lock();
+//		Object	= ((LPMUTUAL_ACCESSIBLE)hObj3D)->Lock();
+//	}
+//	thread_safety::UnlockModule();
+//
+//	if (bResult)
+//	{
+//		bResult = ((LPSCENE3D)Scene)->AddObject((LPOBJECT3D)Object);
+//		((LPMUTUAL_ACCESSIBLE)hObj3D)->Unlock();
+//		((LPMUTUAL_ACCESSIBLE)hScene)->Unlock();
+//	}
+//
+//	return bResult;
+//}
+//
+//BOOL WINAPI SceneDeleteObject(HSCENE hScene, HOBJECT3D hObj3D)
+//{
+//	BOOL bResult = FALSE;
+//
+//	LPUNKNOWN Scene			= NULL;
+//	LPUNKNOWN Object		= NULL;
+//
+//	thread_safety::LockModule(INFINITE);
+//	if ( bResult = _IS_HANDLE_VALID(SCENE3D, hScene) 
+//		&& _IS_HANDLE_VALID(OBJECT3D, hObj3D) )
+//	{
+//		Scene	= ((LPMUTUAL_ACCESSIBLE)hScene)->Lock();
+//		Object	= ((LPMUTUAL_ACCESSIBLE)hObj3D)->Lock();
+//	}
+//	thread_safety::UnlockModule();
+//
+//	if (bResult)
+//	{
+//		// TODO: Implement another DeleteObject methods if necessary
+//		bResult = ((LPSCENE3D)Scene)->DeleteObject(((LPOBJECT3D)Object)->objID);
+//		((LPMUTUAL_ACCESSIBLE)hObj3D)->Unlock();
+//		((LPMUTUAL_ACCESSIBLE)hScene)->Unlock();
+//	}
+//
+//	return bResult;
+//}
+//
+//BOOL WINAPI SceneGetFirstObject(HSCENE hScene, CLASS_ID clsID)
+//{
+//	BOOL bResult;
+//
+//	LPUNKNOWN Master = NULL;
+//
+//	thread_safety::LockModule(INFINITE);
+//
+//	if (bResult = thread_safety::IsObjectRegistered((size_t)hScene, typeid(SCENE3D)))
+//		thread_safety::LockObjectRegistered((size_t)hScene, Master);
+//
+//	thread_safety::UnlockModule();
+//
+//	if (bResult)
+//	{
+//		((LPSCENE3D)Master)->getFirstObject(clsID);
+//
+//		thread_safety::UnlockObjectRegistered((size_t)hScene, Master);
+//	}
+//
+//	return bResult;
+//}
 
 // TODO: implement CScene findObjectIndex, getFirstObject, getObject methods
--- GDIEngine3D/thread_safety.cpp
+++ GDIEngine3D/thread_safety.cpp
@@ -4,111 +4,192 @@
 #include "thread_safety.h"
 
 // ============================================================================
-// CMutualAccessible partial implementation
-
-CMutualAccessible::CMutualAccessible(LPUNKNOWN lpUnknwnObj) : lpObj(lpUnknwnObj)
-{
-	if (lpObj == NULL) throw std::invalid_argument(
-		std::string(typeid(*this).name()).append("::CMutualAccessible: lpUnknwnObj is NULL.").c_str()
-		);
-
-	hMutex = CreateMutex(NULL, FALSE, NULL);
-	if (hMutex == NULL) throw std::bad_exception(
-		std::string(typeid(*this).name()).append("::CMutualAccessible: CreateMutex failed.").c_str()
-		);
-}
-
-CMutualAccessible::~CMutualAccessible()
-{
-	Lock();
-	CloseHandle(hMutex);
-}
-
-// ============================================================================
 // thread_safety namespace methods implementation
 
 #if (defined _DEBUG)
 #define _INITIALIZATION_CHECK \
-	if ( hModuleMutex != NULL ) throw std::bad_exception("thread_safety is not initialized properly!")
+	if ( hModuleMutex == NULL ) throw std::bad_exception("thread_safety is not initialized properly!")
 #else
 #define _INITIALIZATION_CHECK
 #endif
+
+#define _CHECK_ITERATOR(iterator) \
+	iterator != alModuleObjects.end() && iterator != alModuleObjects.begin()
 
 namespace thread_safety
 {
 	namespace 
 	{
+		typedef struct _tagMutualAccessor
+		{
+			HANDLE		_hMutex;
+			LPUNKNOWN	_lpObject;
+
+			inline _tagMutualAccessor()
+			{
+				_hMutex		= NULL;
+				_lpObject	= NULL;
+			}
+		} MUTUAL_ACCESSOR, &REFMUTUAL_ACCESSOR;
+
+		typedef std::map<size_t, MUTUAL_ACCESSOR> OBJECT_REGISTRY;
+
 		HANDLE			hModuleMutex		= NULL; 
-		ACCESSORS_LIST	alModuleObjects;
+		OBJECT_REGISTRY	alModuleObjects;
 	}
+
 
 	BOOL InitializeHandles()
 	{
-		return (hModuleMutex = CreateMutex(NULL, FALSE, _T("GDIEngine3D_Library"))) != NULL;
+		if (hModuleMutex == NULL)
+		{
+			alModuleObjects.insert(OBJECT_REGISTRY::value_type(0, MUTUAL_ACCESSOR()));
+			return (hModuleMutex = CreateMutex(NULL, FALSE, _T("GDIEngine3D_Library"))) != NULL;
+		}
+
+		return TRUE;
 	}
 
 	BOOL ReleaseHandles()
 	{
 		_INITIALIZATION_CHECK;
 
-		return CloseHandle(hModuleMutex) != 0;
+		LockModule(INFINITE);
+		return CloseHandle(hModuleMutex);
 	}
 
-	DWORD thread_safety::LockModule(DWORD dwMilliseconds)
+	DWORD LockModule(DWORD dwMilliseconds)
 	{
 		_INITIALIZATION_CHECK;
 
 		return WaitForSingleObject(hModuleMutex, dwMilliseconds);
 	}
 
-	BOOL thread_safety::UnlockModule()
+	BOOL UnlockModule()
 	{
 		_INITIALIZATION_CHECK;
 
 		return ReleaseMutex(hModuleMutex);
 	}
-	
-	BOOL AccessorExists(LPMUTUAL_ACCESSIBLE obj)
+
+	size_t RegisterObject(LPUNKNOWN lpObj)
 	{
 		_INITIALIZATION_CHECK;
 
-		return std::find(alModuleObjects.begin(), alModuleObjects.end(), obj) != alModuleObjects.end();
+		if (lpObj != NULL)
+		{
+			size_t uObjKey = alModuleObjects.size();
+
+			if (alModuleObjects.find(uObjKey) != alModuleObjects.end())
+			{
+				uObjKey = 0;
+
+				for (OBJECT_REGISTRY::iterator 
+						cur = ++alModuleObjects.begin(), 
+						end = alModuleObjects.end(); 
+					cur != end; 
+					cur++)
+				{
+					if (++uObjKey != cur->first) break;
+				}
+			}
+
+			if (uObjKey != SIZE_MAX) 
+			{
+				OBJECT_REGISTRY::_Pairib pbInsertResult 
+					= alModuleObjects.insert(OBJECT_REGISTRY::value_type(uObjKey, MUTUAL_ACCESSOR()));
+				
+				if (pbInsertResult.second)
+				{
+					pbInsertResult.first->second._lpObject	= lpObj;
+					pbInsertResult.first->second._hMutex	= CreateMutex(NULL, FALSE, NULL);
+					
+					if (pbInsertResult.first->second._hMutex == NULL)
+					{
+						alModuleObjects.erase(pbInsertResult.first);
+					}
+					else
+					{
+						return pbInsertResult.first->first;
+					}
+				}
+			}
+		}
+		else
+		{
+			SetLastError(ERROR_BAD_ARGUMENTS);
+		}
+
+		return SIZE_MAX;
 	}
 
-	BOOL AddAccessor(LPMUTUAL_ACCESSIBLE obj)
+	LPUNKNOWN DeregisterObject(size_t uIndex)
 	{
-		_INITIALIZATION_CHECK;
+		OBJECT_REGISTRY::iterator finder = alModuleObjects.find(uIndex);
 
-		if (obj == NULL || AccessorExists(obj)) return FALSE;
-		
-		alModuleObjects.push_back(obj);
-		return TRUE;
+		if (_CHECK_ITERATOR(finder))
+		{
+			WaitForSingleObject(finder->second._hMutex, INFINITE);
+			if (CloseHandle(finder->second._hMutex))
+			{
+				LPUNKNOWN lpObject = finder->second._lpObject;
+				alModuleObjects.erase(finder);
+				return lpObject;
+			}
+		}
+
+		return NULL;
 	}
 
-	BOOL RemoveAccessor(LPMUTUAL_ACCESSIBLE obj)
+	BOOL IsObjectRegistered(size_t uIndex, const type_info &typeID)
 	{
-		_INITIALIZATION_CHECK;
-
-		size_t libObjectsCount = alModuleObjects.size();
-		alModuleObjects.remove(obj);
-
-		return alModuleObjects.size() != libObjectsCount;					
+		OBJECT_REGISTRY::iterator finder = alModuleObjects.find(uIndex);
+		return _CHECK_ITERATOR(finder) && typeid(*(finder->second._lpObject)) == typeID;
 	}
 
-	VOID ForeachAccessor(VOID (*_foreach_func)(LPMUTUAL_ACCESSIBLE))
+	BOOL LockObjectRegistered(size_t uIndex, LPUNKNOWN &lpObject)
+	{
+		OBJECT_REGISTRY::iterator finder = alModuleObjects.find(uIndex);
+
+		if (_CHECK_ITERATOR(finder))
+		{
+			WaitForSingleObject(finder->second._hMutex, INFINITE);
+			lpObject = finder->second._lpObject;
+
+			return TRUE;
+		}
+
+		return FALSE;
+	}
+
+	BOOL UnlockObjectRegistered(size_t uIndex, LPUNKNOWN &lpObject)
+	{
+		OBJECT_REGISTRY::iterator finder = alModuleObjects.find(uIndex);
+
+		if (_CHECK_ITERATOR(finder) && finder->second._lpObject == lpObject)
+		{
+			lpObject = NULL;
+
+			return ReleaseMutex(finder->second._hMutex);
+		}
+
+		return FALSE;
+	}
+
+	VOID ForeachObjectRegistered(VOID (*_foreach_func)(LPUNKNOWN))
 	{
 		_INITIALIZATION_CHECK;
 
 		if (_foreach_func == NULL) return;
 
-		for ( thread_safety::ACCESSORS_LIST::iterator 
-				cur = thread_safety::alModuleObjects.begin(), 
+		for ( thread_safety::OBJECT_REGISTRY::iterator 
+				cur = ++thread_safety::alModuleObjects.begin(), 
 				end = thread_safety::alModuleObjects.end(); 
 			  cur != end;
 			  cur++
 			)
 		{
-			_foreach_func(*cur);
+			_foreach_func(cur->second._lpObject);
 		}
 	}
 }
